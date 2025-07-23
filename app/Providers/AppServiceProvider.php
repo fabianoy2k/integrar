613a826c38830db4bbf8838c127f5b2da27cd9f3
Fabiano Carvalho da Silva <fabiano@iconeweb.com.br>
2025-07-23 13:58:13 -0300

    ajustes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(config('app.env') === 'production') {
+        // Detecta se está atrás de um proxy HTTPS
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
             \URL::forceScheme('https');
         }
+        
+        // Ou force sempre HTTPS se você sabe que sempre será HTTPS
+        // \URL::forceScheme('https');
     }
 }
